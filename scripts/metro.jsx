b95012ed599cfcaee0a5b74e78d904102dd8b8fd
Christopher Gioia <christophergioia64@gmail.com>
2025-12-24 12:36:04 -0800

    a bunch of cosmetic changes

--- scripts/metro.jsx
+++ scripts/metro.jsx
@@ -760,7 +760,16 @@ function MetroAreaPopulationArea(props) {
                 <th className="table_col_img"></th>
                 <th className="table_col_attribute"></th>
                 <th className="table_col_total">Total</th>
-                <th className="table_col_total_rank">Rank</th>
+                <th className="table_col_total_rank">
+                    Rank
+                    <span className="rank-info-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" viewBox="0 0 16 16" style={{verticalAlign: 'middle', marginLeft: '4px', cursor: 'help'}}>
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                            <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+                        </svg>
+                        <span className="rank-tooltip">Rank indicates the position of this metropolitan area compared to all other metropolitan areas. Lower numbers indicate higher rankings (e.g., Rank 1 is the highest).</span>
+                    </span>
+                </th>
                 <th className="table_col_other"></th>
              </tr>
               <tr>
